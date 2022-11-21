b917767b369efd1f9cd1fa062b09bb1bf2ef7d57
koushiksarkerDIU <antusarker411@gmail.com>
2022-11-22 00:35:26 +0600

    resistration section  done

--- src/components/Pages/Login/Register/Register.js
+++ src/components/Pages/Login/Register/Register.js
@@ -1,32 +1,87 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
+    const { createUser, updateUserProfile, verifyEmail, signInWithGoogle } = useContext(AuthContext);
+    const [error, setError] = useState('')
+
     const handleRegister = (e) => {
         e.preventDefault();
         const form = e.target;
-        const userName = form.name.value;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(userName, email, password);
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                handleUpdateUserProfile(name, photoURL)
+                handleEmailVerify();
+                setError('');
+                form.reset();
+                toast.success('please verify your email')
+                console.log(user);
+            })
+            .catch((error) => {
+                console.error(error);
+                setError(error.message)
+            })
+    }
+    const handleUpdateUserProfile = (name, photoURL) => {
+        const profile = {
+            displayName: name,
+            photoURL: photoURL,
+        }
+        updateUserProfile(profile)
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
+
+    const handleEmailVerify = () => {
+        verifyEmail()
+            .then(() => { })
+            .catch(error => setError(error))
+    }
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+            .then((result) => {
+                const user = result.user;
+                // console.log(user);
+                setError('')
+            }).catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.customData.email;
+                setError(errorCode, errorMessage, email);
+            });
     }
     return (
         <div className="w-2/4 mx-auto p-8 space-y-3 rounded-xl bg-gray-100 dark:text-gray-100">
             <h1 className="text-2xl font-bold text-center">Log In</h1>
             <form onSubmit={handleRegister} className="space-y-6 ng-untouched ng-pristine ng-valid">
                 <div className="space-y-1 text-sm">
-                    <label htmlFor="username" className="block dark:text-gray-400">Username</label>
-                    <input type="text" name="name" id="name" placeholder="Enter your Name" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                    <label htmlFor="username" className="block dark:text-gray-400">Enter Full name</label>
+                    <input type="text" name="name" placeholder="Enter your full Name" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                </div>
+                <div className="space-y-1 text-sm">
+                    <label htmlFor="username" className="block dark:text-gray-400">Upload your photoURL</label>
+                    <input type="text" name="photoURL" placeholder="upload your photoURL" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
                 </div>
                 <div className="space-y-1 text-sm">
                     <label htmlFor="email" className="block dark:text-gray-400">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Enter your email" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                    <input type="email" name="email" placeholder="Enter your email" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
                 </div>
                 <div className="space-y-1 text-sm">
                     <label htmlFor="password" className="block dark:text-gray-400">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                    <input type="password" name="password" placeholder="Password" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
                     <div className="flex justify-end text-xs dark:text-gray-400">
                         <Link href="#">Forgot Password?</Link>
+                    </div>
+                    <div className='text-red-400 font-semibold'>
+                        {error}
                     </div>
                 </div>
                 <button type='submit' className="block w-1/4 mx-auto p-3 text-center rounded-lg dark:text-gray-900 bg-blue-300">Sign in</button>
@@ -37,7 +92,7 @@ const Register = () => {
                 <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
             </div>
             <div className="flex justify-center space-x-4">
-                <button aria-label="Log in with Google" className="p-3 rounded-sm">
+                <button onClick={handleGoogleSignIn} aria-label="Log in with Google" className="p-3 rounded-sm">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
                         <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
                     </svg>
--- src/components/contexts/AuthProvider/AuthProvider.js
+++ src/components/contexts/AuthProvider/AuthProvider.js
@@ -1,17 +1,34 @@
-import { Children, createContext } from "react";
-import { getAuth } from 'firebase/auth'
+import { createContext } from "react";
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, sendEmailVerification, signInWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth'
 import app from "../../Firebase/firebase.config";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-const AuthProvider = () => {
+const AuthProvider = ({ children }) => {
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const updateUserProfile = (profile) => {
+        return updateProfile(auth.currentUser, profile)
+    }
+    const verifyEmail = () => {
+        return sendEmailVerification(auth.currentUser)
+    }
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
     const authInfo = {
-        displayName: 'akash',
+        createUser,
+        updateUserProfile,
+        verifyEmail,
+        signInWithGoogle,
     }
     return (
         <AuthContext.Provider value={authInfo}>
-            {Children}
+            {children}
         </AuthContext.Provider>
     );
 };
